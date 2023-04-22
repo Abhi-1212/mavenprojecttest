803adf45df2db154d6a5b7afcb7bf5737a6c6961
abhishek <abhishek@DESKTOP-OJIJEQU>
2023-04-22 11:10:56 +0530

    my maven project

---
+++ src/test/java/examp/Testabhi.java
@@ -0,0 +1,12 @@
+package examp;
+
+import org.testng.annotations.Test;
+
+public class Testabhi {
+@Test
+	public void firtse() {
+		// TODO Auto-generated method stub
+		System.out.println("hello abhishek");
+	}
+
+}
---
+++ src/test/java/examp/Testmyproect.java
@@ -0,0 +1,23 @@
+package examp;
+
+import org.testng.annotations.Test;
+
+public class Testmyproect {
+
+	@Test
+	public void firt() {
+		// TODO Auto-generated method stub
+for(int i=0;i<=3;i++)
+{
+	for(int j=0;j<=3;j++)
+		System.out.println(i+" "+j);
+}
+	
+	
+	}
+
+
+	
+
+
+}
